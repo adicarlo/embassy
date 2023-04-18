8f938316c317f95fdee7969ea31075f567ab99a6
Adam Di Carlo <a.p.dicarlo@gmail.com>
2023-04-17 23:45:01 -0400

    LED is not on PIN 25 anymore on the Pico W; I happened to wire it onto Pin 15, exactly matching the characteristics of the native LED, which is now controlled by the wifi chip

--- examples/rp/src/bin/blinky.rs
+++ examples/rp/src/bin/blinky.rs
@@ -12,7 +12,7 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let mut led = Output::new(p.PIN_15, Level::Low);
     let dur = Duration::from_millis(1000);
 
     loop {
